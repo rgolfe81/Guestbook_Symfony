a5eeb38e8ed2e71f7d1c00440617678b235afe67
Ruben <rgolfe81@gmail.com>
2023-11-03 18:41:42 +0100

    Add events and use slugs

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -32,7 +32,7 @@ class ConferenceController extends AbstractController
         ]));
     }
 
-    #[Route("/conference/{id}", name: "conference")]
+    #[Route("/conference/{slug}", name: "conference")]
     public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository)
     {
         $offset = max(0, $request->query->getInt('offset', 0));
